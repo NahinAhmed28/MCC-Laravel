9815dd3a42ffc79fccbc54f049004fedc6b51f72
hafez_abdullah_al_noman <noman@mcc.com.bd>
2021-10-31 16:15:08 +0600

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,36 @@
-@extends('layouts.app')
+@include('layouts.app')
+@extends('sidebarTemplate')
 
 @section('content')
 
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                {{-- <div class="card-body">
+                <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }} --}}
+                    {{ __('You are logged in!') }}
                    
                 </div>
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 
 
-@include('sidebarTemplate')
+
+
+<div>
+  <x-header componentName="Dashboard" />
+
+    <h3> Dashboard </h3>
+    </div>
+
 @endsection
